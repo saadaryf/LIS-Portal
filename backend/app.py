f2d244350c581e1ddd5b7e3c1b0dac069b6737c5
Saad Arif <saadaryf@gmail.com>
2024-11-30 21:30:12 +0500

    add logging in wsgi.py

--- backend/app.py
+++ backend/app.py
@@ -17,3 +17,4 @@ def create_app():
     app.register_blueprint(fee_bp, url_prefix='/api')
 
     return app
+
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,6 +1,10 @@
+import logging
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    try:
+        app.run(debug=True)
+    except Exception as e:
+        logging.error(f"Error while running the app: {str(e)}")
