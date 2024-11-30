c2d6f547eea90ffe8a046b5e9060b1490043ff9f
Saad Arif <saadaryf@gmail.com>
2024-11-30 15:29:49 +0500

    update CORS

--- backend/app.py
+++ backend/app.py
@@ -2,14 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={
-    r"/*": {
-        "origins": [
-            "https://lis.devsaura.com/",
-            "http://localhost:3000"
-        ]
-    }
-})
+CORS(app)
 
 
 @app.route('/api/hello', methods=["GET"])
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   const API_URL = process.env.REACT_APP_API_URL;
-  console.log("api url", API_URL)
+  console.log("api url", API_URL);
 
   useEffect(() => {
     fetch(`${API_URL}/api/hello`)
