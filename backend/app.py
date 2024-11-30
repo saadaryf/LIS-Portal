f25154fe077bcd78f4ce395aa04fd0700f1e0366
Saad Arif <saadaryf@gmail.com>
2024-11-30 14:43:06 +0500

    added vercel config for backend deployment

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello():
     return {"message": "Hello from Flask!"}
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
