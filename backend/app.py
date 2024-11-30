0caa374d9e5814dfcd70c70974ca034c2f0fa00e
Saad Arif <saadaryf@gmail.com>
2024-11-30 15:12:41 +0500

    env for deployment on cloudflare

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,21 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={
+    r"/*": {
+        "origins": [
+            "https://lis.devsaura.com/",
+            "http://localhost:3000"
+        ]
+    }
+})
+
 
 @app.route('/api/hello', methods=["GET"])
 def hello():
     return {"message": "Hello from Flask!"}
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,15 @@ import React, { useEffect, useState } from "react";
 function App() {
   const [message, setMessage] = useState("");
 
+  const API_URL = process.env.REACT_APP_API_URL;
+  console.log("api url", API_URL)
+
   useEffect(() => {
-    fetch("/api/hello")
+    fetch(`${API_URL}/api/hello`)
       .then((response) => response.json())
-      .then((data) => setMessage(data.message));
-  }, []);
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, [API_URL]);
 
   return <div>{message}</div>;
 }
