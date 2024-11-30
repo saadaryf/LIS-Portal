086fe6fc2e2d197eed855b53fd8016e688d9b000
Saad Arif <saadaryf@gmail.com>
2024-11-30 21:01:40 +0500

    add axios in package.json

--- backend/app.py
+++ backend/app.py
@@ -5,18 +5,15 @@ from config.settings import SECRET_KEY, DEBUG
 from app.routes.student_routes import student_bp
 from app.routes.fee_routes import fee_bp
 
+
 def create_app():
     app = Flask(__name__)
-    CORS(app)  
-    
+    CORS(app)
+
     app.config['SECRET_KEY'] = SECRET_KEY
-    
+
     # Register blueprints
     app.register_blueprint(student_bp, url_prefix='/api')
     app.register_blueprint(fee_bp, url_prefix='/api')
-    
-    return app
 
-# if __name__ == "__main__":
-#     app = create_app()
-#     app.run(debug=True)
+    return app
