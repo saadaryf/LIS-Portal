12b884e42b8bd960324eeb1cfbcfa1cc53358839
Saad Arif <saadaryf@gmail.com>
2024-11-30 21:35:49 +0500

    renamed app.py to main.py and update import in wsgi

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,10 +1,6 @@
-import logging
-from app import create_app
+from main import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    try:
-        app.run(debug=True)
-    except Exception as e:
-        logging.error(f"Error while running the app: {str(e)}")
+    app.run(debug=True)
